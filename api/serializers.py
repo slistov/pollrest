ca9b9605c67961b95b741024d123f53a2dd9f4ed
Сергей Листов <slistov@mail.ru>
2021-10-02 02:39:16 +0300

    Refactoring

--- api/serializers.py
+++ api/serializers.py
@@ -16,14 +16,15 @@ class PollSerializer(serializers.ModelSerializer):
         ]
     
     def update(self, instance, validated_data):
-        """Перед сохранением экземпляра бросить исключение
-        при попытке изменить readonly-поле.
+        """Бросить исключение при попытке изменить readonly-поле.
 
         По задаче: у опроса после создания нельзя изменять start_date
         """
-        if instance.start_date != validated_data.get('start_date', instance.start_date):
+        if instance.start_date != validated_data.get('start_date', 
+                                        instance.start_date):
             raise models.ProtectedError("Нельзя изменять поле 'start_date'", None)
         else:
+            # Вызвать метод родителя
             return serializers.ModelSerializer.update(self, instance, validated_data)
 
 
